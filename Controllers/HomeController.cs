853a73874cb1a09051944f8da322f60a7c6f8eb9
gotesjo <120645559+gotesjo@users.noreply.github.com>
2023-12-13 10:30:20 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WEB_test.Models;
+
+namespace WEB_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult startpage()
+        {
+            return View();
+        }
+
+        public IActionResult editProfile()
+        {
+            return View();
+        }
+
+        public IActionResult edit()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
